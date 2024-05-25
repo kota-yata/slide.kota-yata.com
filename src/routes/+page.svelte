57deb164dc246749bf9781f5212435ac098fbb4f
Kota Yatagai <kota@yatagai.com>
2024-05-25 22:54:34 +0900

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<!-- <meta name="description" content="Svelte demo app" /> -->
+</svelte:head>
+
+<section>
+	<h2>
+		wassup
+	</h2>
+</section>
+
+<style lang="scss">
+  h2 {
+    margin: 0;
+  }
+</style>
